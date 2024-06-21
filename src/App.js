6af0842b3c3a12be592209d913c9bc70fe5e4c33
justinjames92 <nj9428947@gmail.com>
2024-06-20 23:36:36 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [radius, setRadius] = useState(0);
+
+  const handleInput = (e) => {
+    setRadius(parseInt(e.target.value));
+  };
+//Function to print circles
+  const getCircle = () => {
+    let circles=[];
+    for (let i = 10; i <= radius; i += 10) {
+        circles.push(<circle key={i} cx="200" cy="150" r={i} stroke="white" strokeWidth="2" fill="none" />);
+    }
+    return circles
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <label>Radius</label>
+        <input
+          type="number"
+          name="radius"
+          value={radius}
+          onChange={handleInput}
+        />
+        {radius ? (<svg width="400" height="400"> {getCircle()} </svg>):(<p>Enter the radius</p>) }
       </header>
     </div>
   );
